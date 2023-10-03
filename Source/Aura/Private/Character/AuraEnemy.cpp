ca85609540240fbed630d67eb9d645b78d345b5d
Katie Thompson <katie@codingkat.dev>
2023-10-03 15:45:36 -0700

    Created EnemyInterface class, and implemented highlight effect on cursor hover.

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -1,2 +1,12 @@
 #include "Character/AuraEnemy.h"
 
+
+void AAuraEnemy::HighlightActor()
+{ 
+	bHighlighted = true;
+}
+
+void AAuraEnemy::UnHighlightActor()
+{ 
+	bHighlighted = false;
+}
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,11 +1,19 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 
 AAuraPlayerController::AAuraPlayerController()
 { 
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick( float DeltaTime )
+{ 
+	Super::PlayerTick( DeltaTime );
+
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -48,5 +56,62 @@ void AAuraPlayerController::Move( const FInputActionValue &InputActionValue )
 	{
 		ControlledPawn->AddMovementInput( ForwardDirection, InputAxisVector.Y );
 		ControlledPawn->AddMovementInput( RightDirection, InputAxisVector.X );
+	}
+}
+
+void AAuraPlayerController::CursorTrace()
+{ 
+	FHitResult CursorHit;
+	GetHitResultUnderCursor( ECC_Visibility, false, CursorHit );
+	if( !CursorHit.bBlockingHit ) return;
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>( CursorHit.GetActor() );
+
+	/**
+	* Line trace from cursor. There are several scenarios:
+	* A. LastActor is null && ThisActor is null
+	*	- Do nothing
+	* B. LastActor is null && ThisActor is valid
+	*	- Highlight ThisActor
+	* C. LastActor is valid && ThisActor is null
+	*	- UnHightlight LastActor
+	* D. Both actors are valid, but LastActor != ThisActor
+	*	- UnHightlight LastActor, and Highlight ThisActor
+	* E. Both actors are valid, and are the same actor
+	*	- Do nothing
+	**/
+
+	//if( LastActor == nullptr && ThisActor == nullptr ) return; // Case A
+	//else if( LastActor == nullptr && ThisActor != nullptr ) // Case B
+	//{
+	//	ThisActor->HighlightActor();
+	//}
+	//else if( LastActor != nullptr && ThisActor == nullptr ) // Case C
+	//{
+	//	LastActor->UnHighlightActor();
+	//}
+	//else if( LastActor != nullptr && ThisActor != nullptr ) 
+	//{
+	//	if( LastActor != ThisActor ) // Case D
+	//	{
+	//		LastActor->UnHighlightActor();
+	//		ThisActor->HighlightActor();
+	//	}
+	//	else // LastActor == ThisActor // Case E
+	//		return;		
+	//}
+
+
+	if( ThisActor != LastActor )
+	{
+		if( LastActor != nullptr )
+		{
+			LastActor->UnHighlightActor();
+		}
+		if( ThisActor != nullptr )
+		{
+			ThisActor->HighlightActor();
+		}
 	}
 }
