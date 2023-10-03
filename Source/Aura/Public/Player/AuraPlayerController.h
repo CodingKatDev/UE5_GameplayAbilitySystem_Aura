31f15c61809740c413b8e0e1172d4dc7979c201d
Katie Thompson <katie@codingkat.dev>
2023-10-02 19:09:30 -0700

    Added and bound Move functionality to Input Action. AuraPlayerController BP created.

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -4,7 +4,10 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+
 class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 
 UCLASS()
@@ -17,8 +20,14 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 
 private:
 	UPROPERTY( EditAnywhere, Category = "Input" )
 	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY( EditAnywhere, Category = "Input" )
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move( const FInputActionValue &InputActionValue );
 };
