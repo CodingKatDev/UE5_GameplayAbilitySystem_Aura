bddc1cb2aa75c8a430a11016135aefaf41711836
Katie Thompson <katie@codingkat.dev>
2023-10-11 13:04:39 -0700

    Added handling for infinite GE in AuraEffectActor.

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -2,14 +2,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameplayEffectTypes.h"
 #include "AuraEffectActor.generated.h"
 
 
 class UGameplayEffect;
+class UAbilitySystemComponent;
 
 
 UENUM( BlueprintType )
-enum class EEffectApplicationPolicy
+enum class EEffectApplicationPolicy : uint8
 {
 	ApplyOnOverlap,
 	ApplyOnEndOverlap,
@@ -17,7 +19,7 @@ enum class EEffectApplicationPolicy
 };
 
 UENUM( BlueprintType )
-enum class EEffectRemovalPolicy
+enum class EEffectRemovalPolicy : uint8
 {
 	RemoveOnEndOverlap,
 	DoNotRemove
@@ -68,4 +70,6 @@ protected:
 
 	UPROPERTY( EditAnywhere, BlueprintReadOnly, Category = "Applied Effects" )
 	EEffectRemovalPolicy InfiniteEffectRemovalPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
+
+	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent *> ActiveEffectHandles;
 };
