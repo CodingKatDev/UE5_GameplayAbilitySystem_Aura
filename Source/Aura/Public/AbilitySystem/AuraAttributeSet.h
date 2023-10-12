5acbd35427b82a6be78f1de66b6ada9b0e1570b6
Katie Thompson <katie@codingkat.dev>
2023-10-11 18:08:51 -0700

    Added framework to easily access and change GE properties for source and target.

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,6 +13,35 @@
  	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 
+USTRUCT()
+struct FEffectProperties
+{
+	GENERATED_BODY()
+
+	FEffectProperties(){ }
+
+	FGameplayEffectContextHandle EffectContextHandle;
+
+	UPROPERTY()
+	UAbilitySystemComponent *SourceASC = nullptr;
+	UPROPERTY()
+	AActor *SourceAvatarActor = nullptr;
+	UPROPERTY()
+	AController *SourceController = nullptr;
+	UPROPERTY()
+	ACharacter *SourceCharacter = nullptr;
+
+	UPROPERTY()
+	UAbilitySystemComponent *TargetASC = nullptr;
+	UPROPERTY()
+	AActor *TargetAvatarActor = nullptr;
+	UPROPERTY()
+	AController *TargetController = nullptr;
+	UPROPERTY()
+	ACharacter *TargetCharacter = nullptr;
+};
+
+
 UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
@@ -22,6 +51,8 @@ public:
 	UAuraAttributeSet();
 	virtual void GetLifetimeReplicatedProps( TArray<FLifetimeProperty> &OutLifetimeProps ) const override;
 
+	virtual void PreAttributeChange( const FGameplayAttribute &Attribute, float &NewValue )override;
+	virtual void PostGameplayEffectExecute( const FGameplayEffectModCallbackData &Data )override;
 
 	UPROPERTY( BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes" )
 	FGameplayAttributeData Health;
@@ -39,7 +70,6 @@ public:
 	FGameplayAttributeData MaxMana;
 	ATTRIBUTE_ACCESSORS( UAuraAttributeSet, MaxMana );
 
-
 	UFUNCTION()
 	void OnRep_Health( const FGameplayAttributeData OldHealth ) const;
 
@@ -51,4 +81,7 @@ public:
 
 	UFUNCTION()
 	void OnRep_MaxMana( const FGameplayAttributeData &OldMaxMana ) const;
+
+private:
+	void SetEffectProperties( const FGameplayEffectModCallbackData &Data, FEffectProperties &Props ) const;
 };
