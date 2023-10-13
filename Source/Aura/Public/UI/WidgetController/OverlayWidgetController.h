c288aaac5dabff1c71d07cdc973ab2e5ed6545e7
Katie Thompson <katie@codingkat.dev>
2023-10-13 11:51:06 -0700

    Broadcasting from Message Data Table Rows

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -7,15 +7,8 @@
 #include "OverlayWidgetController.generated.h"
 
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FOnHealthChangedSignature, float, NewHealth );
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FOnMaxHealthChangedSignature, float, NewMaxHealth );
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FOnManaChangedSignature, float, NewMana );
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FOnMaxManaChangedSignature, float, NewMaxMana );
-
-
 class UAuraUserWidget;
 struct FOnAttributeChangeData;
-
 
 USTRUCT( BlueprintType )
 struct FUIWidgetRow : public FTableRowBase
@@ -33,7 +26,15 @@ struct FUIWidgetRow : public FTableRowBase
 
 	UPROPERTY( EditAnywhere, BlueprintReadOnly )
 	UTexture2D *Image = nullptr;
-}; 
+};
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FOnHealthChangedSignature, float, NewHealth );
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FOnMaxHealthChangedSignature, float, NewMaxHealth );
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FOnManaChangedSignature, float, NewMana );
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FOnMaxManaChangedSignature, float, NewMaxMana );
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( FMessageWidgetRowSignature, FUIWidgetRow, Row );
 
 
 UCLASS( BlueprintType, Blueprintable )
@@ -56,6 +57,9 @@ public:
 
 	UPROPERTY( BlueprintAssignable, Category = "GAS|Attributes" )
 	FOnMaxManaChangedSignature OnMaxManaChanged;
+
+	UPROPERTY( BlueprintAssignable, Category = "GAS|Messages" )
+	FMessageWidgetRowSignature MessageWidgetRow;
 
 protected:
 	UPROPERTY( EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data" )
