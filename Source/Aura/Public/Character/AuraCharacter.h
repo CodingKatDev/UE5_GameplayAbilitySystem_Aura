c08bad81478c6d4c575c8ccea2a8b018f3dd3dc5
Katie Thompson <katie@codingkat.dev>
2023-10-18 17:42:07 -0700

    Created Combat Interface, and getter for Player Level for furture attribute calculations.

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -20,6 +20,11 @@ public:
 	virtual void PossessedBy( AController *NewController ) override;
 	virtual void OnRep_PlayerState() override;
 
+	//~ Begin Combat Interface
+	virtual int32 GetPlayerLevel() override;
+
+	//~ End Combat Interface
+
 private:
 	UPROPERTY( VisibleAnywhere, Category = "Camera" )
 	TObjectPtr<USpringArmComponent> CameraArm;
