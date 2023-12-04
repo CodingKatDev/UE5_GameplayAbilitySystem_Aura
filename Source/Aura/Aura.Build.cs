863f92107e4a191905ab488f1e1e7a2acc2899bf
Katie Thompson <katie@codingkat.dev>
2023-12-04 13:24:18 -0800

    Projectile impact sound and effect.

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -10,7 +10,7 @@ public class Aura : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "UMG", "GameplayTags" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTasks", "NavigationSystem" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTasks", "NavigationSystem", "Niagara" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
