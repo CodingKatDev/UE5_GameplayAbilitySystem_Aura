2a50d4e715bd520fc83e38ebad4605ba1d6d40d0
Katie Thompson <katie@codingkat.dev>
2024-01-05 18:40:11 -0800

    Enemy Health Progess Bar

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -3,7 +3,11 @@
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
 #include "Interaction/EnemyInterface.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
 #include "AuraEnemy.generated.h"
+
+
+class UWidgetComponent;
 
 
 UCLASS()
@@ -22,9 +26,13 @@ public:
 
 	//~ Begin Combat Interface
 	virtual int32 GetPlayerLevel() override;
-
 	//~ End Combat Interface
 
+	UPROPERTY( BlueprintAssignable )
+	FOnAttributeChangedSignature OnHealthChanged;
+
+	UPROPERTY( BlueprintAssignable )
+	FOnAttributeChangedSignature OnMaxHealthChanged;
 
 protected:
 	virtual void BeginPlay() override;
@@ -32,4 +40,7 @@ protected:
 
 	UPROPERTY( EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults" )
 	int32 Level = 1;
+
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly )
+	TObjectPtr<UWidgetComponent> HealthBar;
 };
