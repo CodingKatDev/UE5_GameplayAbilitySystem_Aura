0b46b32a9bf8229ed01c2efea7609698a8b72f1d
Katie Thompson <katie@codingkat.dev>
2024-02-15 17:34:08 -0800

    Ability Info Data Asset

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -7,6 +7,7 @@
 #include "OverlayWidgetController.generated.h"
 
 
+class UAbilityInfo;
 class UAuraUserWidget;
 struct FOnAttributeChangeData;
 
@@ -60,6 +61,9 @@ public:
 protected:
 	UPROPERTY( EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data" )
 	TObjectPtr<UDataTable> MessageWidgetDataTable;
+
+	UPROPERTY( EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data" )
+	TObjectPtr<UAbilityInfo>AbilityInfo;
 
 	template<typename T>
 	T *GetDataTableRowByTag( UDataTable *DataTable, const FGameplayTag &Tag );
