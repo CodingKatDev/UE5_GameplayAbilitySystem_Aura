9bc870ec137ae5ce29944ca7d8df027ff053348d
Katie Thompson <katie@codingkat.dev>
2024-02-22 16:07:05 -0800

    Add/Set XP on Player State, broadcast & listen for changes to update player level, HUD XP bar percent updates.

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -1,13 +1,17 @@
 #pragma once
 
-#include "CoreMinimal.h"
 #include "AbilitySystemInterface.h"
+#include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
 #include "AuraPlayerState.generated.h"
 
 
 class UAbilitySystemComponent;
 class UAttributeSet;
+class ULevelUpInfo;
+
+
+DECLARE_MULTICAST_DELEGATE_OneParam( FOnPlayerStatChanged, int32 /*StatValue*/ )
 
 
 UCLASS()
@@ -21,7 +25,20 @@ public:
 	virtual UAbilitySystemComponent *GetAbilitySystemComponent() const override;
 	UAttributeSet *GetAttributeSet() const { return AttributeSet; }
 
+	UPROPERTY( EditDefaultsOnly )
+	TObjectPtr<ULevelUpInfo> LevelUpInfo;
+
+	FOnPlayerStatChanged OnXPChangedDelegate;
+	FOnPlayerStatChanged OnLevelChangedDelegate;
+
 	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+	FORCEINLINE int32 GetXP() const { return XP; }
+
+	void AddToXP( int32 InXP );
+	void AddToLevel( int32 InLevel );
+
+	void SetXP( int32 InXP );
+	void SetLevel( int32 InLevel );
 
 protected:
 	UPROPERTY( VisibleAnywhere )
@@ -34,6 +51,12 @@ private:
 	UPROPERTY( VisibleAnywhere, ReplicatedUsing = OnRep_Level )
 	int32 Level = 1;
 
+	UPROPERTY( VisibleAnywhere, ReplicatedUsing = OnRep_XP )
+	int32 XP = 1;
+
 	UFUNCTION()
 	void OnRep_Level( int32 OldLevel );
+
+	UFUNCTION()
+	void OnRep_XP( int32 OldXP );
 };
