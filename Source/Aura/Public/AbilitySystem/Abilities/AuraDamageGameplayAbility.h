6a360cca902df4b51ed5bde5fdb39e8525caf4a6
Katie Thompson <katie@codingkat.dev>
2024-04-02 20:10:02 -0700

    Added ManaCost and Cooldown getters for Spell Menu descriptions. Seperated FireBolt into child of ProjectileSpell.

--- Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
@@ -24,4 +24,6 @@ protected:
 
 	UFUNCTION( BlueprintPure )
 	FTaggedMontage GetRandomTaggedMontageFromArray( const TArray<FTaggedMontage> &TaggedMontages ) const;
+
+	float GetDamageByDamageType( float InLevel, const FGameplayTag &DamageType );
 };
